e876bd5b0c4624783cc7d7aaf23c8c1c68ed53b8
Alvin Yanson <yansonalvin29@gmail.com>
2021-10-02 11:11:48 +0800

    mintable token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
-contract DappToken is StandardToken, DetailedERC20 {
+
+contract DappToken is MintableToken, PausableToken, DetailedERC20 {
     constructor(
         string _name,
         string _symbol,
